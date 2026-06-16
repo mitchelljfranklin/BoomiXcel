60eea42e9b3c764592675477e2a90f069fc1acb1
Mitchell Franklin <mitchellfranklin@gmail.com>
2026-06-16 20:42:18 +1000

    feat: dynamically update 'View in Process Reporting' href on hover
    Added mouseenter handler that re-computes the link href from current URL parameters, so the link stays accurate even if componentIdOnFocus changes after page load.

--- src/library/boomiapp/content/headerActions.js
+++ src/library/boomiapp/content/headerActions.js
@@ -127,5 +127,16 @@ document.arrive('[data-locator="link-description"]', { existing: true }, functio
   link.target = '_blank';
   link.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style="width: 24px; height: 24px;"><title>View in Process Reporting</title><path d="M22 12H18L15 21L9 3L6 12H2" stroke="#8C8C8C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>';
 
+  link.addEventListener('mouseenter', function () {
+    var currentId = getUrlParameter("componentIdOnFocus");
+    if (!currentId) return;
+    var processReportingEl = document.querySelector('[data-locator="link-process-reporting"]');
+    var accountId =
+      getUrlParameter("accountId") ||
+      (processReportingEl && processReportingEl.href.split("=").pop().split(";")[0]);
+    if (!accountId) return;
+    link.href = 'https://platform.boomi.com/AtomSphere.html#reporting;accountId=' + accountId + ';processes=' + currentId;
+  });
+
   descLink.insertAdjacentElement('afterend', link);
 });
